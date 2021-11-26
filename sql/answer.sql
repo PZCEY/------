b3a220f35645633c578569c2bc9dc04187daf869
xf-Zheng <xianfangz@student.unimelb.edu.au>
2021-11-27 00:21:28 +0800

    11.27 update 2.0

--- sql/answer.sql
+++ sql/answer.sql
@@ -1,8 +1,17 @@
 use sqlpractice;
-SELECT department, Name, Salary from
-(select ID as eid,Name,Salary, rank() over(order by salary partition by Id) as ranking
+SELECT department, Name, Salary 
+from
+(select departmentid as eid,Name,Salary, rank() over(partition by DepartmentID order by salary DESC) as ranking
 from 
 employee) a left join
 department 
 on a.eid = department.ID
 where ranking = 1;
+
+select d.Department, e.name as Name, e.salary  from 
+department d inner join employee e
+on d.ID = e.DepartmentID
+INNER JOIN 
+(select departmentId, max(salary) as salary from employee
+group by departmentID) c
+ON c.departmentId = e.departmentId and c.salary = e.salary
